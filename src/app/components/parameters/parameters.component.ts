63e87b0c768773a8f0d0c20714a68e0f41bf13ea
FedjaD <durmicfedja@gmail.com>
2017-11-30 14:33:33 +0100

    bind for buttons

--- src/app/components/parameters/parameters.component.ts
+++ src/app/components/parameters/parameters.component.ts
@@ -47,7 +47,45 @@ export class ParametersComponent implements OnInit {
     ]
   },
   {
-    name: "section 1",
+    name: "section 2",
+    formFields: [
+      {
+        id: 0,
+        type: "radio",
+        obj: [
+          {name: 'one', active: true, value: 1},
+          {name: 'two', active: false, value: 2}
+        ]
+      },
+      {
+        id: 1,
+        type: "radio",
+        obj: [
+          {name: 'one', active: true, value: 1},
+          {name: 'two', active: false, value: 2},
+          {name: 'three', active: false, value: 3}
+        ]
+      },
+      {
+        id: 2,
+        type: "radio",
+        obj: [
+          {name: 'one', active: true, value: 1},
+          {name: 'two', active: false, value: 2}
+        ]
+      },
+      {
+        id: 3,
+        type: "select",
+        obj: [
+          {name: 'one', value: 1},
+          {name: 'two', value: 2}
+        ]
+      },
+    ]
+  },
+  {
+    name: "section 3",
     formFields: [
       {
         id: 0,
