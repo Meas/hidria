5b543dc9baf073975d5fb84efbd8a5f385f804e1
FedjaD <durmicfedja@gmail.com>
2017-12-05 16:43:44 +0100

    working graph

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -24,29 +24,27 @@ export class ChartComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
   	var canvas = <HTMLCanvasElement> document.getElementById(this.canvasId);
   	var ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+   
+    var array = [];
+    for(var i = 0; i <= 10; i+=0.01)
+      array.push(Math.round(i * 100)/100);
+    console.log(array);
     var data = {
-      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      labels: array,
         datasets: [{
             label: "f(x) = x",
             function: function(x) { return x },
             borderColor: "rgba(75, 192, 192, 1)",
             data: [],
-            fill: false
+            fill: true
         },
         {
             label: "f(x) = x²",
             function: function(x) { return x*x },
             borderColor: "rgba(153, 102, 255, 1)",
             data: [],
-            fill: false
+            fill: true
         },
-        {
-            label: "f(x) = x * log(x)",
-            function: function(x) { return x*Math.log(x) },
-            borderColor: "rgba(255, 206, 86, 1)",
-            data: [],
-            fill: false
-        }]
     };
 
     Chart.pluginService.register({
@@ -57,69 +55,69 @@ export class ChartComponent implements OnInit, AfterViewInit {
                   var fct = data.datasets[i].function,
                       x = data.labels[j],
                       y = fct(x);
-                    data.datasets[i].data.push(y);
+                    data.datasets[i].data.push(Math.round(y * 100)/100);
                 }
             }
         }
     });
 
-
+    /*data.labels=[0,2,4,6,8,10];*/
     var myBarChart = new Chart(ctx, {
         type: 'line',
         data: data,
         options: {
+            elements: {
+                point: {
+                    radius: 0
+                },
+                line: {
+                  
+                }
+            },
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero:true
+                        beginAtZero:true,
+                        /*userCallback: function(label, index, labels) {
+                             if (label % 20 == 0) {
+                                 return label;
+                             }
+                         },*/
+                         maxTicksLimit: 5,
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                      beginAtZero:true,
+                      /*userCallback: function(label, index, labels) {
+                           if (label % 2 === 0) {
+                               return label;
+                           }
+                       },*/
+                       maxRotation: 0,
+                       maxTicksLimit: 5,
+                       /*beginAtZero: true*/
                     }
                 }]
             },
-            pointRadius: 0,
+            tooltips: {
+               mode: 'index',
+               intersect: false,
+             },
+            hover: {
+              mode: 'nearest',
+              intersect: false
+             },
         }
     });
-
-
-
-  	/*var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-          datasets: [{ 
-              data: [86,114,106,106,107,111,133,221,783,2478],
-              label: "Africa",
-              borderColor: "#3e95cd",
-              fill: false
-            }, { 
-              data: [282,350,411,502,635,809,947,1402,3700,5267],
-              label: "Asia",
-              borderColor: "#8e5ea2",
-              fill: false
-            }, { 
-              data: [168,170,178,190,203,276,408,547,675,734],
-              label: "Europe",
-              borderColor: "#3cba9f",
-              fill: false
-            }, { 
-              data: [40,20,10,16,24,38,74,167,508,784],
-              label: "Latin America",
-              borderColor: "#e8c3b9",
-              fill: false
-            }, { 
-              data: [6,3,2,2,7,26,82,172,312,433],
-              label: "North America",
-              borderColor: "#c45850",
-              fill: false
-            }
-          ]
-        },
-        options: {
-          title: {
-            display: true,
-            text: 'World population per region (in millions)'
-          }
-        }
-      });*/
+    document.getElementById(this.canvasId).onclick = function(evt){
+      console.log(myBarChart.getElementsAtEvent(evt));
+        var activePoints = myBarChart.getElementsAtEvent(evt);
+        var firstPoint = activePoints[1];
+        var label = myBarChart.data.labels[firstPoint._index];
+        var value = myBarChart.data.datasets[firstPoint._datasetIndex].data[firstPoint._index];
+        alert(label + ": " + value);
+    };
   }
 
 }
