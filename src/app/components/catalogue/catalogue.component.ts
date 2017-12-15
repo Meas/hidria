fdeda0c9c88a58fb05aec48901dde3f760ee2e2e
vildantursic <vildan.tursic@walter.ba>
2017-12-15 10:10:32 +0100

    minor changes, adding buttons to form

--- src/app/components/catalogue/catalogue.component.ts
+++ src/app/components/catalogue/catalogue.component.ts
@@ -11,112 +11,7 @@ import {Router} from "@angular/router";
 export class CatalogueComponent implements OnInit {
 
   slug = 'all';
-  sections = [
-    {
-      slug: 'ac_axial_fans',
-      title: 'AC Axial fans',
-      items: [
-        {
-          slug: 'slick_blades',
-          title: 'AC Axial fans | Slick Blades',
-          description: 'Sickled blades',
-          items: [
-            {
-              slug: 'item_1',
-              title: 'Test 1',
-              description: 'Desc',
-              items: []
-            },
-            {
-              slug: 'item_2',
-              title: 'Test 2',
-              description: 'Desc',
-              items: []
-            }
-          ]
-        },
-        {
-          slug: 'slick_blades_1',
-          title: 'AC Axial fans',
-          description: 'Blades with composite material (PP/AP)es',
-          items: []
-        },
-        {
-          slug: 'slick_blades_2',
-          title: 'AC Axial fans',
-          description: 'Paddle blades',
-          items: []
-        },
-        {
-          slug: 'slick_blades_3',
-          title: 'UL listed Axial Fans',
-          description: '',
-          items: []
-        }
-      ]
-    },
-    {
-      slug: 'ec_axial_fans',
-      title: 'EC Axial fans',
-      items: [
-        {
-          slug: 'slick_blades_5',
-          title: 'H-ROOF fans',
-          description: '',
-          items: []
-        },
-        {
-          slug: 'slick_blades_6',
-          title: 'H-TUBE fans',
-          description: '',
-          items: []
-        },
-        {
-          slug: 'slick_blades_7',
-          title: 'SQUARE PLATE fans',
-          description: '',
-          items: []
-        }
-      ]
-    },
-    {
-      slug: 'axial_fans_for_ventialtion',
-      title: 'Axial fans for Ventilation',
-      items: [
-        {
-          slug: 'slick_blades_8',
-          title: 'H-ROOF fans',
-          description: '',
-          items: []
-        },
-        {
-          slug: 'slick_blades_9',
-          title: 'H-TUBE fans',
-          description: '',
-          items: []
-        }
-      ]
-    },
-    {
-      slug: 'ec_axial_fans_2',
-      title: 'EC Axial fans 2',
-      items: [
-        {
-          slug: 'slick_blades_10',
-          title: 'Test',
-          description: 'Desc',
-          items: []
-        },
-        {
-          slug: 'slick_blades_11',
-          title: 'Test',
-          description: 'Desc',
-          items: []
-        }
-      ]
-    }
-  ];
-  feature= [];
+  feature = [];
   subSections = [];
 
   constructor(private catalogueService: CatalogueService, private router: Router) { }
@@ -127,7 +22,8 @@ export class CatalogueComponent implements OnInit {
 
   getItems(): void {
     this.catalogueService.getItems().subscribe((response: any) => {
-      this.feature=response;
+      this.feature = response;
+      console.log(this.feature)
     });
   }
 
