60890e7ef6fd3491b1b919c84d817929c9187487
FedjaD <durmicfedja@gmail.com>
2017-12-20 15:03:50 +0100

    working table from json on choose-model view

--- src/app/shared/tabs/tabs.component.ts
+++ src/app/shared/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
-  constructor() { }
+  @Input() tableData;
+  selectedTable;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit() {
+  	setTimeout(function() {
+  		document.getElementById('tab0').click();
+  	});
+  }
+
+  selectTable(tabId, tableName) {
+  	this.selectedTable=tableName;
+  	var tabs =  document.getElementsByClassName('tab');
+  	for (var i = 0; i < tabs.length; i++) {
+  	    (tabs[i].id == tabId) ? tabs[i].classList.add('active') : tabs[i].classList.remove('active');
+  	}
+  }
+  routeTo(link) {
+  	if(link)
+  		this.router.navigate([link]);
   }
 
 }
