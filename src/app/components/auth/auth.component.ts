fe52db4f00be1bfab6650e5ac69f046e99a9db4c
vildantursic <vildan.tursic@walter.ba>
2018-01-24 21:28:15 +0100

    authentication

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  register = {
+    firstName: '',
+    lastName: '',
+    password: '',
+    email: '',
+    country: '',
+    company: '',
+    address: '',
+    telephone: ''
+  };
+
+  login = {
+    email: '',
+    password: ''
+  };
+
+  resetPassword = {
+    email: ''
+  };
+
+  changePassword = {
+    passwordOld: '',
+    password: ''
+  };
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onRegisterClicked() {
+    this.authService.register(this.register).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
+
+  onLoginClicked() {
+    this.authService.login(this.login).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
+
+  onResetPasswordClicked() {
+    this.authService.resetPassword(this.register).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
+
+  onChangePasswordClicked() {
+    this.authService.chanagePassword(this.register).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import {MainService} from "../main.service";
 
 @Injectable()
 export class AuthService {
 
-  constructor() {}
+  constructor(private service: MainService) {}
 
   /**
    * Is Logged In
@@ -12,5 +13,41 @@ export class AuthService {
    */
   isLoggedIn(): boolean {
     return !!localStorage.getItem('access_token') && localStorage.getItem('access_token') !== undefined;
+  }
+
+  /**
+   * Login
+   * @param data
+   * @returns {Observable<boolean>}
+   */
+  login(data): Observable<any> {
+    return this.service.post('login', data);
+  }
+
+  /**
+   * Register
+   * @param data
+   * @returns {Observable<boolean>}
+   */
+  register(data): Observable<any> {
+    return this.service.post('register', data);
+  }
+
+  /**
+   * Change password
+   * @param data
+   * @returns {Observable<boolean>}
+   */
+  chanagePassword(data): Observable<any> {
+    return this.service.post('change-password', data);
+  }
+
+  /**
+   * Reset password
+   * @param data
+   * @returns {Observable<boolean>}
+   */
+  resetPassword(data): Observable<any> {
+    return this.service.post('reset-password', data);
   }
 }
