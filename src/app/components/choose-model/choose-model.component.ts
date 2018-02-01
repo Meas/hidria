6cce9a74271698ce2c46a354d8d4876f8b62da0a
FedjaD <durmicfedja@gmail.com>
2018-02-01 14:54:27 +0100

    new apis for choose-model and choose-model/operating-point

--- src/app/components/choose-model/choose-model.component.ts
+++ src/app/components/choose-model/choose-model.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ChooseModelService} from '../../services/chooseModel/chooseModel.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { CustomNotificationsService } from '../../services/notifications/notifications.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-choose-model',
@@ -10,15 +14,47 @@ export class ChooseModelComponent implements OnInit {
 
   feature: any = [];
 
-  constructor(private chooseModelService: ChooseModelService) { }
+  constructor(private chooseModelService: ChooseModelService, private activatedRoute: ActivatedRoute,
+              private notifications: CustomNotificationsService) { }
 
   ngOnInit() {
     this.getItems();
   }
 
   getItems(): void {
-    this.chooseModelService.getItems().subscribe((response: any) => {
+    /* this.chooseModelService.getItems().subscribe((response: any) => {
       this.feature = response;
+    }); */
+    const data = this.getDataFromParams();
+    this.chooseModelService.postCustomItems(data).subscribe((response: any) => {
+      if (response.featureObjects) {
+        this.feature = response;
+      } else {
+        this.notifications.getError({'status': 400, 'statusText': 'No results!'});
+      }
     });
   }
+
+  getDataFromParams() {
+    const tempArray: Array<any> = [];
+    this.activatedRoute.queryParams.subscribe((queryParams: Params) => {
+      if (!_.isEmpty(queryParams)) {
+        for (const attribute in queryParams) {
+          if (queryParams[attribute]) {
+            const tempObject = {
+              'name': attribute,
+              'value': queryParams[attribute]
+            };
+            tempArray.push(tempObject);
+          }
+        }
+      } else {
+        this.activatedRoute.params.subscribe((param: Params) => {
+          tempArray.push({'name': 'ID', 'value': param.slug});
+        });
+      }
+    });
+    return tempArray;
+  }
+
 }
--- src/app/services/operating-point/operating-point.service.ts
+++ src/app/services/operating-point/operating-point.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { MainService } from '../main.service';
+
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class OperatingPointService {
 
-  constructor(private service: MainService) { }
+  constructor(private service: MainService, private http: HttpClient) { }
 
   /**
    * Gets array of items
@@ -18,5 +23,11 @@ export class OperatingPointService {
   calculate(event): Observable<any> {
     // ToDo format data from event and send request appropriately
     return this.service.get('choose-model/operating-point/calculate/id');
+  }
+  getCustomItems(id): Observable<any> {
+    return this.http.get(`http://13.93.51.225/hidriaAPI/api/v1/choose-model/operating-point/${id}`)
+    .catch((err: any) => {
+      return Observable.of(err.error);
+    });
   }
 }
